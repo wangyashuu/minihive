2fe7014f36c1d78279b8df211bfd53c0e6ab19c5
wangyashuu <wangyashuu@gmail.com>
2022-01-25 20:51:26 +0000

    add: ms4 3waysjoin and merge select

--- minihive/ra2mr.py
+++ minihive/ra2mr.py
@@ -1,3 +1,4 @@
+import hashlib
 from enum import Enum
 import ast
 import operator
@@ -12,9 +13,42 @@ import radb.ast
 import radb.parse
 from radb.parse import RAParser as sym
 
+
 """
 util function
 """
+
+s = 20
+
+relation_size = {
+    "REGION": s,
+    "NATION": s,
+    "ORDERS": s,
+    "CUSTOMER": s,
+    "LINEITEM": s,
+}
+
+
+def hash_to_buckets(val, size):
+    val = str(val).encode("utf-8")
+    hash = int(hashlib.sha256(val).hexdigest(), 16)
+    return hash % size
+
+
+def match_keys(keys_array):
+    items = [keys for keys in keys_array if [None] not in keys]
+    if len(items) < 1:
+        return False
+    both_keys = items[0]
+    for keys in keys_array:
+        if keys != both_keys and (
+            len([k for k in keys if k == [None]]) > 1
+            or not all(
+                [a == b or a == [None] for a, b in zip(keys, both_keys)]
+            )
+        ):
+            return False
+    return True
 
 
 def decomposite_conjunctive_cond(cond):
@@ -27,22 +61,43 @@ def decomposite_conjunctive_cond(cond):
     return conds
 
 
-def get_cond_target(relation, json, target):
-    if isinstance(target, radb.ast.Literal):  # isinstance
-        return ast.literal_eval(target.val)
-
-    if isinstance(target, radb.ast.AttrRef):
-        if target.rel is None or target.rel == relation:
-            return json[f"{relation}.{target.name}"]
-        elif target.rel is not None and f"{target.rel}.{target.name}" in json:
-            return json[f"{target.rel}.{target.name}"]
+def target_of_cond(relation, json_tuple, cond):
+    for inp in cond.inputs:
+        if isinstance(inp, radb.ast.AttrRef):
+            if inp.rel is None:
+                return json_tuple[f"{relation}.{inp.name}"]
+            elif inp.rel is not None and f"{inp.rel}.{inp.name}" in json_tuple:
+                return json_tuple[f"{inp.rel}.{inp.name}"]
     return None
 
 
-def match_cond(relation, json, cond):
-    left, right = cond.inputs
-    left = get_cond_target(relation, json, left)
-    right = get_cond_target(relation, json, right)
+def targets_of_cond(relation, json_tuple, cond):
+    vals = []
+    for inp in cond.inputs:
+        val = None
+        if isinstance(inp, radb.ast.Literal):  # isinstance
+            val = ast.literal_eval(inp.val)
+        if isinstance(inp, radb.ast.AttrRef):
+            if inp.rel is None:
+                val = json_tuple[f"{relation}.{inp.name}"]
+            elif inp.rel is not None and f"{inp.rel}.{inp.name}" in json_tuple:
+                val = json_tuple[f"{inp.rel}.{inp.name}"]
+        vals.append(val)
+    return vals
+
+
+def target_of_conds(relation, json_tuple, conds):
+    return [
+        [
+            target_of_cond(relation, json_tuple, c)
+            for c in decomposite_conjunctive_cond(cond)
+        ]
+        for cond in conds
+    ]
+
+
+def match_cond(relation, json_tuple, cond):
+    left, right = targets_of_cond(relation, json_tuple, cond)
     comparison_ops = {
         sym.LT: operator.lt,  # "<"
         sym.LE: operator.le,  # "<="
@@ -66,17 +121,22 @@ def is_map_only_job(task):
     return False
 
 
+def get_attr_rels(cond, schema=None):
+    if schema is None:
+        return set([i.rel for i in cond.inputs if type(i) == radb.ast.AttrRef])
+
+
 def get_rels(rel):
     if type(rel) == radb.ast.RelRef:
         return set([rel.rel])
     rel_list = [get_rels(i) for i in rel.inputs]
-    if type(rel) == radb.ast.Rename:
-        rel_list = [{rel.relname}]
     return set.union(*rel_list)
 
 
 def optimize_task(cls):
     class Wrapper(cls):  # cls
+        optimize_join = False
+
         def __init__(self, *args, **kwargs):
             super().__init__(*args, **kwargs)
             self.unwrap_requires = super().requires
@@ -94,7 +154,7 @@ def optimize_task(cls):
                         raquery = radb.parse.one_statement_from_string(
                             task.querystring
                         )
-                        rels = str(list(get_rels(raquery)))
+                        rels = str(sorted(get_rels(raquery)))
                         branches[rels] = [task] + branches.get(rels, [])
                         queue += task.unwrap_requires()
                     else:
@@ -104,27 +164,125 @@ def optimize_task(cls):
             self.branches = branches
             self.subtasks = subtasks
 
-            ## rule 2. cat after map only jobs with reducer.
-
-            ## rule 3. reorder after filter to front. (DONE by pushing select)
+            # 3-ways joins
+            if (
+                self.optimize
+                and len(self.subtasks) == 2
+                and self.optimize_join
+            ):
+                subtasks = []
+                subjoin = None
+                for t in self.subtasks:
+                    if isinstance(t, RelAlgQueryTask):
+                        subsubtasks = t.requires()
+                        if len(subsubtasks) == 2:
+                            subtasks += subsubtasks
+                            subjoin = t
+                            self.branches.update(t.branches)
+                            continue
+                    subtasks.append(t)
+                if len(subtasks) == 3:
+                    self.subjoin = subjoin
+                    self.subtasks = subtasks
+                    self.unwrap_mapper = self.three_way_joins_mapper
+                    self.reducer = self.three_way_joins_reducer
 
         def requires(self):
             return self.subtasks
 
         def mapper(self, line):
+            lines = [line]
             relation, tuple = line.split("\t")
             branches = [v for k, v in self.branches.items() if relation in k]
             if len(branches) > 0:
                 merged_subtasks = branches[0]
                 for t in merged_subtasks:
-                    subtask_result = next(t.unwrap_mapper(line), None)
-                    if subtask_result is None:
-                        return
-                    line = "\t".join(subtask_result)
+                    new_lines = []
+                    for l in lines:
+                        new_lines += ["\t".join(o) for o in t.unwrap_mapper(l)]
+                    lines = new_lines
 
-            result = next(super().mapper(line), None)
-            if result is not None:
-                yield result
+            for l in lines:
+                for out in self.unwrap_mapper(l):
+                    yield out
+
+        def three_way_joins_mapper(self, line):
+            relation, tuple = line.split("\t")
+            json_tuple = json.loads(tuple)
+            conditions = [
+                radb.parse.one_statement_from_string(q).cond
+                for q in [self.subjoin.querystring, self.querystring]
+            ]
+            rels_array = [get_attr_rels(cond) for cond in conditions]
+            rels = [
+                next(
+                    iter(cur.difference(*[r for r in rels_array if r != cur]))
+                )
+                for cur in rels_array
+            ]
+            keys = target_of_conds(relation, json_tuple, conditions)
+            empty_indices = [
+                i for i, key in enumerate(keys) if all(k is None for k in key)
+            ]
+
+            if len(empty_indices) > 0:
+                idx = empty_indices[0]
+                bucket_size = relation_size[rels[idx]]
+
+                for i in range(bucket_size):
+                    output_key = [
+                        hash_to_buckets(key, relation_size[r])
+                        if j != idx
+                        else i
+                        for j, (r, key) in enumerate(zip(rels, keys))
+                    ]
+                    yield (
+                        json.dumps(output_key),
+                        json.dumps([relation, json_tuple]),
+                    )
+
+            else:
+                output_key = [
+                    hash_to_buckets(key, relation_size[r])
+                    for r, key in zip(rels, keys)
+                ]
+                yield (
+                    json.dumps(output_key),
+                    json.dumps([relation, json_tuple]),
+                )
+
+        def three_way_joins_reducer(self, key, values):
+            conditions = [
+                radb.parse.one_statement_from_string(q).cond
+                for q in [self.subjoin.querystring, self.querystring]
+            ]
+
+            data = [json.loads(val_str) for val_str in values]
+            rel_names = {item[0] for item in data}
+            keys = json.loads(key)
+            if len(rel_names) == len(keys) + 1:
+                relations = {
+                    rel: [v for r, v in data if r == rel] for rel in rel_names
+                }
+                output_key = str(sorted(rel_names))
+                one, two, three = list(relations.values())
+                for one_t in one:
+                    for two_t in two:
+                        for three_t in three:
+                            compare_keys = [
+                                target_of_conds(
+                                    relation, json_tuple, conditions
+                                )
+                                for relation, json_tuple in zip(
+                                    rel_names, [one_t, two_t, three_t]
+                                )
+                            ]
+                            if match_keys(compare_keys):
+                                obj = dict()
+                                obj.update(one_t)
+                                obj.update(two_t)
+                                obj.update(three_t)
+                                yield (output_key, json.dumps(obj))
 
     return Wrapper
 
@@ -299,44 +457,30 @@ class JoinTask(RelAlgQueryTask):
     def mapper(self, line):
         relation, tuple = line.split("\t")
         json_tuple = json.loads(tuple)
-
         raquery = radb.parse.one_statement_from_string(self.querystring)
-        condition = raquery.cond
-
-        """ .................. fill in your code below ...................."""
-        conds = decomposite_conjunctive_cond(condition)
-        key = [
-            get_cond_target(relation, json_tuple, c.inputs[0])
-            or get_cond_target(relation, json_tuple, c.inputs[1])
-            for c in conds
-        ]
-
-        yield (json.dumps(key), json.dumps({relation: json_tuple}))
-
-        """ .................. fill in your code above ...................."""
+        conditions = [raquery.cond]
+        keys = target_of_conds(relation, json_tuple, conditions)
+        yield (
+            json.dumps(keys),
+            json.dumps([relation, json_tuple]),
+        )
 
     def reducer(self, key, values):
-        raquery = radb.parse.one_statement_from_string(self.querystring)
+        data = [json.loads(val_str) for val_str in values]
+        rel_names = {item[0] for item in data}
+        keys = json.loads(key)
 
-        """ ................. fill in your code below ..................."""
-
-        relations = dict()
-        for val_str in values:
-            val = json.loads(val_str)
-            rel, obj = next(iter(val.items()))
-            relations[rel] = relations.get(rel, []) + [obj]
-
-        if len(relations) == 2:
-            left, right = list(relations.values())
-            for left_t in left:
-                for right_t in right:
+        if len(rel_names) == len(keys) + 1:
+            relations = {
+                rel: [v for r, v in data if r == rel] for rel in rel_names
+            }
+            one, two = list(relations.values())
+            for one_t in one:
+                for two_t in two:
                     obj = dict()
-                    obj.update(left_t)
-                    obj.update(right_t)
-                    yield ("foo", json.dumps(obj))
-                    # print("for debug")
-
-        """ ................. fill in your code above ..................."""
+                    obj.update(one_t)
+                    obj.update(two_t)
+                    yield (str(sorted(rel_names)), json.dumps(obj))
 
 
 @optimize_task
